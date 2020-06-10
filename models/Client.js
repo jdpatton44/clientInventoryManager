ad943ae2c3da3762e5581ceacf65eb4a053652a2
jdpatton44 <jdpattonv@gmail.com>
2020-06-10 15:34:03 -0400

    initial commit

---
+++ models/Client.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const clientSchema = new mongoose.Schema(
+    {
+        created: {
+            type: Date,
+            default: Date.now,
+          },
+          clientName: {
+            type: String,
+            trim: true,
+            required: "Please enter the client's Name",
+          },
+          clientAbrv: {
+            type: String,
+            trim: true,
+            required: 'Please enter a abbreviation for the client.',
+          },
+    }
+);
+
+module.exports = mongoose.model('Client', clientSchema);
